12244617cb6575be3fef1e6838a731bca2c704a8
Lindy Lewis <lindylewis@lindys-mbp.attlocal.net>
2018-05-11 14:28:28 -0500

    Added functionality to read in text from files and search for strings on the same line

--- lib/search.rb
+++ lib/search.rb
@@ -12,9 +12,25 @@ class Search
 
 	def search_for_strings
 		array_of_files = get_filepaths_from_dir
-		array_of_files.each do |file|
-			puts file
+		
+
+		array_of_files.each do |file_in_list|
+			puts "Now searching#{file_in_list}."
+		  
+      File.open(file_in_list, 'r') do |file|
+  			while line = file.gets
+  				
+					if line.include? @string1
+						if line.include? @string2
+							puts "Found inside #{file_in_list}: #{line}"
+						end
+					end
+
+				end
+			end
+
 		end
+
 	end
 
 end
