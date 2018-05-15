0f7051df818040b61798eada09f22106f012eeb0
Lindy Lewis <lindylewis@lindys-mbp.attlocal.net>
2018-05-15 10:59:31 -0500

    Created better acceptance test:Given, When, Then

--- lib/search.rb
+++ lib/search.rb
@@ -1,4 +1,5 @@
 class Search
+
 	def initialize(distance, directory_path, string1, string2)
 		@distance = distance
 		@directory_path = directory_path
@@ -7,22 +8,22 @@ class Search
 	end
 
 	def get_filepaths_from_dir
-		Dir.glob("#{@directory_path}/*.txt")	
+		Dir.glob("#{@directory_path}/*.txt")
   end
 
 	def search_for_strings
 		array_of_files = get_filepaths_from_dir
-		
 
 		array_of_files.each do |file_in_list|
-			puts "Now searching#{file_in_list}."
-		  
+			#puts "Now searching #{file_in_list}."
+
       File.open(file_in_list, 'r') do |file|
   			while line = file.gets
-  				
+
 					if line.include? @string1
 						if line.include? @string2
-							puts "Found inside #{file_in_list}: #{line}"
+							#puts "Found inside #{file_in_list}: #{line}"
+							return file_in_list.to_s
 						end
 					end
 
@@ -31,6 +32,6 @@ class Search
 
 		end
 
-	end
 
+	end
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,11 +1,28 @@
 require 'search'
 
+# Given a directory containing one or more text files
+# When I execute the program
+#  and supply the directory path
+#  and provide the search distance
+#  and provide the two strings
+# Then the program should return the set of documents where
+#    the strings are located within the specified distance of one another
+
 describe Search do
 
-  it "The first file returned should be ironbars.txt" do
+  it "Finds three text files in the directory" do
     test_search = Search.new(2, 'test_data', 'my first string', 'my second string')
     filepaths = test_search.get_filepaths_from_dir
-    expect(filepaths[0]).to eq "test_data/ironbars.txt"
+    expect(filepaths.length).to eq 3
   end
+
+  it "Returns the filepath of a locality search in the same document" do
+    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
+    context_files = test_search.search_for_strings
+    expect(context_files).to eq 'test_data/citychickens.txt'
+  end
+
+
+
 
 end
