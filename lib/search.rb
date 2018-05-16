951c8c2333fa778088c3370913d211b434a72257
Lindy Lewis <lindylewis@lindys-mbp.attlocal.net>
2018-05-15 21:02:25 -0500

    searches for string1 in text document

--- lib/search.rb
+++ lib/search.rb
@@ -1,43 +1,82 @@
 class Search
+	attr_reader :distance, :directory_path, :string1_array, :string2_array
 
 	def initialize(distance, directory_path, string1, string2)
 		@distance = distance
 		@directory_path = directory_path
-		@string1 = string1
-		@string2 = string2
+		@string1_array = clean_array(string1.split)
+		@string2_array = clean_array(string2.split)
+		@results = []
 	end
 
 	def get_filepaths_from_dir
-		return Dir.glob("#{@directory_path}/*.txt")
+		array_of_filepaths = Dir.glob("#{@directory_path}/*.txt")
+		if array_of_filepaths.empty?
+			#puts "Did not find any files in #{@directory_path}."
+		else
+			#puts "Searching these files: #{array_of_filepaths}"
+		end
+		return array_of_filepaths
   end
 
+	def clean_array(array)
+		array.map! do |string|
+			string.downcase.gsub(/[^a-z0-9\s]/i, '')
+		end
+		return array
+	end
+
 	def get_array_of_words_from_file(file_path)
-		array_of_words = File.open( file_path ) do |file|
+		text_array = File.open( file_path ) do |file|
 			file.read.split
 		end
+		return clean_array(text_array)
 	end
 
-	def get_array_of_words_from_string(string)
-		return string.split
-	end
-
-	def get_index_of_first_match(array_to_search, array_to_find)
+	def string1_within_text_array?(array_to_search)
+		length_of_string1_array = @string1_array.length
 		index = 0
+
 		array_to_search.each do |word|
-			if array_to_find[0] == word
-				return index
+			if @string1_array[0] == word
+				sliced_array = array_to_search.slice(index, length_of_string1_array)
+				puts " "
+				puts "index: #{index}"
+				puts "sliced_array: #{sliced_array}"
+				puts "@string1_array: #{@string1_array}"
+				puts " "
+				if sliced_array == @string1_array
+					puts "breaking..."
+					return index
+				else
+					index = index + 1
+				end
+			else
+				index = index + 1
 			end
-			index = index + 1
 		end
+
+		return -1
 	end
 
-	def search_for_array_within_array(array_to_search, array_to_find)
-		index = get_index_of_first_match(array_to_search, array_to_find)
-		length = array_to_find.length
-		new_array = array_to_search.slice(index, length)
-		new_array == array_to_find
-	end
 
+
+
+
+=begin
+		def get_index_of_string1_location(array_to_search)
+			index = 0
+			array_to_search.each do |word|
+				if @string1_array[0] == word
+					return index
+				end
+				index = index + 1
+			end
+			return index
+		end
+	=end
+
+=begin
 	def get_array_of_index_ranges(array_to_search, index, locality)
 		index - locality < 0 ? first_index = 0 : first_index = index - locality
 		index + locality > array_to_search.length - 1 ? last_index = array_to_search.length : last_index = index + locality
@@ -45,7 +84,6 @@ class Search
 	end
 
 	def search_array_by_index_ranges(array_to_search, array_to_find, index_array)
-
 		is_found = false
 		length = array_to_find.length
 
@@ -55,12 +93,48 @@ class Search
 				is_found = true
 			end
 		end
-
 		return is_found
-
 	end
 
+	def perform_locality_search
 
+		array_of_filepaths = get_filepaths_from_dir
+		array_to_find = @string1_array
 
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		#puts " "
+		#puts "@distance: #{@distance}"
+		#puts "@directory_path: #{@directory_path}"
+		#puts "@string1: #{@string1}"
+		#puts "@string2: #{@string2}"
+		#puts "array_of_filepaths: #{array_of_filepaths}"
+		#puts "array_to_find: #{array_to_find}"
 
+		array_of_filepaths.each do |filepath|
+
+			array_to_search = get_array_of_words_from_file(filepath)
+			puts " "
+			puts "searching #{filepath}......"
+
+			contains_string1 = string1_within_text_array?(array_to_search, @string1_array)
+			puts "String 1 is found in #{filepath}:  #{contains_string1}"
+
+			if contains_string1
+				index_of_first_match = get_index_of_first_word_in_string1(array_to_search, array_to_find)
+				array_of_index_ranges = get_array_of_index_ranges(array_to_search, index_of_first_match, @distance)
+				contains_string2 = search_array_by_index_ranges(array_to_search, @string2_array, array_of_index_ranges)
+				if (contains_string2 == true)
+					puts "inside results if statement: #{filepath}"
+					@results << filepath
+				end
+			end
+		end
+
+		puts "Match found for #{@string1} and #{@string2} in the following file: #{@results}"
+		return true
+	end
+=end
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -10,68 +10,103 @@ require 'search'
 
 describe Search do
 
-  it "Finds all text files in a directory" do
-    test_search = Search.new(2, 'test_data', 'my first string', 'my second string')
+  it "Lists all text files in a directory when the given directory contains files" do
+    test_search = Search.new(2, 'test_data', 'Cattleya and Logan', 'best kitties')
     filepaths = test_search.get_filepaths_from_dir
-    expect(filepaths.length).to eq 3
+    expect(filepaths.length).to eq 4
   end
 
-  it "Creates an array containing the words of a file" do
-    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
-    array_of_words = test_search.get_array_of_words_from_file('test_data/citychickens.txt')
-    expect(array_of_words[4]). to eq 'Hanna'
+  it "Displays a message when the directory does not exist" do
+    test_search = Search.new(2, 'fake_directory', 'Cattleya and Logan', 'best kitties')
+    filepaths = test_search.get_filepaths_from_dir
+    expect(filepaths).to eq []
   end
 
-  it "Gets the index of the first word in the string" do
-    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
-    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
-    array_to_find = ["a", "time"]
-    index = test_search.get_index_of_first_match(array_to_search, array_to_find)
-    expect(index). to eq 2
+  it "Creates an array containing the words from a file" do
+    test_search = Search.new(2, 'test_data', 'Cattleya and Logan', 'best kitties')
+    array_of_words = test_search.get_array_of_words_from_file('test_data/kitties.txt')
+    expect(array_of_words[12]). to eq 'logan'
   end
 
-  it "Checks the rest of the substring for a match" do
-    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
-    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
-    array_to_find = ["in", "Neverland"]
-    is_match = test_search.search_for_array_within_array(array_to_search, array_to_find)
-    expect(is_match). to eq true
+  it "Cleans an array of special characters" do
+    test_search = Search.new(2, 'test_data', 'Cattleya and Logan', 'best kitties')
+    text_array = test_search.get_array_of_words_from_file('test_data/kitties.txt')
+    expect(text_array[12]). to eq 'logan'
   end
 
-  it "Checks the rest of the substring for a match" do
-    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
-    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
-    array_to_find = ["in", "time"]
-    is_match = test_search.search_for_array_within_array(array_to_search, array_to_find)
-    expect(is_match). to eq false
+  it "Returns location of string1 in test_array or -1 if not found" do
+    test_search = Search.new(2, 'test_data', 'Cattleya and Logan', 'best kitties')
+    text_array = test_search.get_array_of_words_from_file('test_data/kitties.txt')
+    is_string_found = test_search.string1_within_text_array?(text_array)
+    expect(is_string_found). to eq 10
   end
 
-  it "Gets an array of index ranges" do
-    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
-    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
-    array_to_find = ["in", "Neverland"]
-    index = 1
-    locality = 4
-    is_found = test_search.get_array_of_index_ranges(array_to_search, index, locality)
-    expect(is_found). to eq [0, 1, 2, 3, 4, 5]
+  it "Returns location of string1 in test_array or -1 if not found" do
+    test_search = Search.new(2, 'test_data', 'Cattleya and Tim', 'best kitties')
+    text_array = test_search.get_array_of_words_from_file('test_data/kitties.txt')
+    is_string_found = test_search.string1_within_text_array?(text_array)
+    expect(is_string_found). to eq -1
   end
 
-  it "Searches an array based on index range" do
-    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
-    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
-    array_to_find = ["in", "Neverland"]
-    index_array = [0, 1, 2, 3, 4, 5]
-    is_found = test_search.search_array_by_index_ranges(array_to_search, array_to_find, index_array)
-    expect(is_found). to eq true
-  end
 
-  it "Searches an array based on index range" do
-    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
-    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
-    array_to_find = ["in", "Lindy"]
-    index_array = [0, 1, 2, 3, 4, 5]
-    is_found = test_search.search_array_by_index_ranges(array_to_search, array_to_find, index_array)
-    expect(is_found). to eq false
-  end
+
+
+#  it "Should return true if array_to_find is found within array_to_search" do
+#    test_search = Search.new(2, 'test_data', 'in Neverland', 'French')
+#    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
+#    array_to_find = ["in", "Neverland"]
+#    is_match = test_search.string1_within_text_array?(array_to_search)
+#    expect(is_match). to eq true
+#  end
+#
+#  it "Should return false if array_to_find is not found within array_to_search" do
+#    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
+#    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
+#    array_to_find = ["in", "time"]
+#    is_match = test_search.string1_within_text_array?(array_to_search)
+#    expect(is_match). to eq false
+#  end
+#
+#  it "Gets an array of index ranges" do
+#    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
+#    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
+#    array_to_find = ["in", "Neverland"]
+#    index = 1
+#    locality = 4
+#    is_found = test_search.get_array_of_index_ranges(array_to_search, index, locality)
+#    expect(is_found). to eq [0, 1, 2, 3, 4, 5]
+#  end
+#
+#  it "Searches an array based on index range" do
+#    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
+#    array_to_find = ["in", "Neverland"]
+#    index_array = [0, 1, 2, 3, 4, 5]
+#    is_found = test_search.search_array_by_index_ranges(array_to_search, array_to_find, index_array)
+#    expect(is_found). to eq true
+#  end
+
+#  it "Searches an array based on index range" do
+#    test_search = Search.new(2, 'test_data', 'I kinda', 'French')
+#    array_to_search = ["Once", "upon", "a", "time", "in", "Neverland"]
+#    array_to_find = ["in", "Lindy"]
+#    index_array = [0, 1, 2, 3, 4, 5]
+#    is_found = test_search.search_array_by_index_ranges(array_to_search, array_to_find, index_array)
+#    expect(is_found). to eq false
+#  end
+
+#  it "Display an array when search terms are found" do
+#    test_search = Search.new(6, 'test_data', 'When Dot', 'Penny')
+#    expect(test_search.perform_locality_search). to eq true
+#  end
+
+#  it "Display an empty array if no match is found." do
+#    test_search = Search.new(6, 'test_data', 'Tim', 'Lindy')
+#    expect(test_search.perform_locality_search). to eq true
+#  end
+
+#  it "Display an empty array if only one match is found" do
+#    test_search = Search.new(6, 'test_data', 'chemistry', 'lab partner')
+#    expect(test_search.perform_locality_search). to eq true
+#  end
 
 end
